2c9c0ee15915185aff139203a589c21a2aaf1bef
Michael Klein <michael@firstiwaslike.com>
2012-08-29 09:30:38 +0200

    install real oh-my-zsh instead of fork

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,10 @@
 cd ~
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# install customization
+cp ~/.vim/aliases.zsh ~/.oh-my-zsh/lib/aliases.zsh
+cp ~/.vim/mik.zsh-theme ~/.oh-my-zsh/themes/mik.zsh-theme
+# symlinks
 ln -s ~/.vim/vimrc.local ~/.vimrc
 ln -s ~/.vim/zshrc.local ~/.zshrc
 ln -s ~/.vim/tmux.conf.local ~/.tmux.conf
