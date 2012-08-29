74bc15b80e55e70808008d6106e7d4723a46e206
Michael Klein <michael@firstiwaslike.com>
2012-08-29 09:19:43 +0200

    add aliases and mik theme

---
+++ aliases.zsh
@@ -0,0 +1,46 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+
+#alias g='grep -in'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias lsa='ls -lah'
+alias l='ls -la'
+alias ll='ls -l'
+alias sl=ls # often screw this up
+
+alias afind='ack-grep -il'
+
+#reload .zshrc
+alias rld='source ~/.zshrc'
+
+# edit oh_my_zsh
+alias ozsh='mvim ~/.oh-my-zsh/'
+alias zshrc='mvim ~/.zshrc'
+
+# work
+alias runtastic='cd ~/development/runtastic/runtastic-web/'
+alias restore='cd ~/development/runtastic/restore/'
+
+# mvim 
+alias m="mvim"
+
+# redis
+alias redis="redis-server /usr/local/etc/redis.conf"
+alias dipl="cd ~/Dropbox/studium/Diplomarbeit/public_cloud_risks/"
+
+# tmux
+alias t="tmux"
+alias tl="tmux ls"
+alias ta="tmux attach -t"
---
+++ mik.zsh-theme
@@ -0,0 +1,19 @@
+# Colors
+autoload -U colors
+colors
+setopt prompt_subst
+
+# Save an indicator to a local variable if the last command exited with success.
+local csuccess="%{$reset_color%}%(?,%{$fg[green]%}»%{$reset_color%},%{$fg[red]%}»%{$reset_color%})"
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[white]%}±%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_SUFFIX=" "
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}✗%{%f%k%b%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=" %{%F{green}%}✓%{%f%k%b%}"
+
+
+PROMPT='
+%~ $(git_prompt_info) %{$reset_color%}
+${csuccess} %{$reset_color%}'
+# right prompt
+RPROMPT='%{$fg_bold[red]%}$(rvm_prompt_info) %{$reset_color%}'
