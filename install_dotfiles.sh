bbf0f4f9ef796ba6490e81012cbd893b64b6c0ed
Michael Klein <michael@firstiwaslike.com>
2013-01-05 11:25:11 +0100

    Improve installation script
      Vundle and oh-my-zsh will now be reinstalled every time we run
      the installation script. Also we make sure vi bindings are
      working in terminal and ruby consoles after running installation.
    
      - cleanup before installing dotfiles
      - add ackrc editrc and inputrc to install script
      - small cleanup in vundle bundles

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,13 +1,19 @@
 cd ~
+# cleanup
+rm -rf ~/.oh-my-zsh ~/.vim/bundle
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 # install customization
 cp ~/.vim/aliases.zsh ~/.oh-my-zsh/lib/aliases.zsh
 cp ~/.vim/mik.zsh-theme ~/.oh-my-zsh/themes/mik.zsh-theme
-# symlinks
 cp ~/.vim/vimrc.local ~/.vimrc
 cp ~/.vim/zshrc.local ~/.zshrc
 cp ~/.vim/tmux.conf.local ~/.tmux.conf
+cp ~/.vim/ackrc.local ~/.ackrc
+# make vi mode work in terminal
+cp ~/.vim/inputrc.local ~/.inputrc
+# make vi mode work in irb/pry/rails_console
+cp ~/.vim/editrc.local ~/.editrc
 # to install vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 # to install bundles
