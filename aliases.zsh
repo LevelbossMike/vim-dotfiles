ff2f539fda0f547e397d462034356c5efd2464e0
Michael Klein <michael@firstiwaslike.com>
2014-05-08 13:26:56 +0200

    Update zsh aliases and install script.
    I want to be able to update oh-my-zsh so I put the custom
    stuff (theme and aliases) in the custom dir where it belongs.

--- aliases.zsh
+++ aliases.zsh
@@ -1,45 +1,4 @@
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
-
-# Basic directory operations
-alias ...='cd ../..'
-alias -- -='cd -'
-
-# Super user
-alias _='sudo'
-
-#alias g='grep -in'
-
-# Show history
-alias history='fc -l 1'
-
-# List direcory contents
-alias lsa='ls -lah'
-alias l='ls -la'
-alias ll='ls -l'
-alias sl=ls # often screw this up
-
-alias afind='ack-grep -il'
-
-#reload .zshrc
-alias rld='source ~/.zshrc'
-
-# edit oh_my_zsh
-alias ozsh='mvim ~/.oh-my-zsh/'
-alias zshrc='mvim ~/.zshrc'
-
-# work
-alias runtastic='cd ~/development/runtastic/runtastic-web/'
-alias restore='cd ~/development/runtastic/restore/'
-
-# mvim 
-alias m="mvim"
-
-# redis
-alias redis="redis-server /usr/local/etc/redis.conf"
-
-# tmux 
+# tmux
 alias t="tmux -u" # run with utf-8 enabled!
 alias tl="tmux ls"
 alias ta="tmux attach -t"
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,8 +4,8 @@ rm -rf ~/.oh-my-zsh ~/.vim/bundle
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 # install customization
-cp ~/.vim/aliases.zsh ~/.oh-my-zsh/lib/aliases.zsh
-cp ~/.vim/mik.zsh-theme ~/.oh-my-zsh/themes/mik.zsh-theme
+cp ~/.vim/aliases.zsh ~/.oh-my-zsh/custom
+mkdir ~/.oh-my-zsh/custom/themes && cp ~/.vim/mik.zsh-theme ~/.oh-my-zsh/custom/themes
 cp ~/.vim/vimrc.local ~/.vimrc
 cp ~/.vim/zshrc.local ~/.zshrc
 cp ~/.vim/tmux.conf.local ~/.tmux.conf
